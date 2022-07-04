463dfa7839f3e0f1b2901ff57d96d2214ede8fad
AbanoubAmir <abanoub889@gmail.com>
2022-07-04 14:19:34 +0200

    Add project files.

---
+++ Student_Affairs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Student_Affairs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
